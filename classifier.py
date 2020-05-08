9e6b90f800b5a9dc221c7d2c9adc25bcd77e56fc
tutu <15186846+xueyuanl@users.noreply.github.com>
2020-05-08 08:59:43 -0700

    first submit

---
+++ classifier.py
@@ -0,0 +1,80 @@
+import logging
+import os
+import yaml
+from random import randint
+
+from PIL import UnidentifiedImageError
+
+from label import label_image
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s: %(message)s', level=logging.INFO)
+logging.getLogger('classifier').setLevel(level=logging.WARNING)
+
+with open('conf.yaml') as f:
+    path = yaml.safe_load(f)['path']
+
+
+def create_folder(ff):
+    if not os.path.exists(ff):
+        os.makedirs(ff)
+
+
+def class_photo(s_file, t_file):
+    logging.info('move {} to {}'.format(s_file, t_file))
+    os.system('mv {} {}'.format(s_file, t_file))
+
+
+def pick_up_photo(folder):
+    files = os.listdir(folder)
+    file_number = len(files)
+    if file_number < 1:
+        return None
+    logging.info('get the total number of files is {}.'.format(file_number))
+    index = randint(0, file_number - 1)
+    return files[index]
+
+
+def format_filename(file):
+    pass
+
+
+def append_rate_to_file_name(rate, file):
+    r = str(rate)[2:5]
+    tmp = file.split('.')
+    new_file = tmp[0] + '_' + r + '.' + tmp[1]
+    return new_file
+
+
+def main():
+    while True:
+        # this file name is supposed not to include space or other illegal characters
+        # otherwise, implement format_filename func to filter them.
+        file = pick_up_photo(path)
+        logging.info('picked up the photo file name is {}'.format(file))
+        file_path = os.path.join(path, file)
+        if os.path.isdir(file_path):
+            continue
+        logging.info('file_path is {}'.format(file_path))
+        try:
+            rate, label = label_image(input_mean=0, input_std=255, model_file='new_mobile_model.tflite',
+                                      label_file='class_labels.txt', image=file_path)
+            logging.info('get class is: {}, rate: {}'.format(label, rate))
+            class_path = os.path.join('classified_data', label)
+            new_file_name = append_rate_to_file_name(rate, file)
+            class_photo(file_path, os.path.join(class_path, new_file_name))
+        except UnidentifiedImageError as e:
+            logging.info(e)
+            class_path = os.path.join(path, 'error_image')
+            create_folder(class_path)
+            logging.info('move error image {} to {}'.format(file_path, class_path))
+            class_photo(file_path, class_path)
+        except ValueError as e:
+            logging.info(e)
+            class_path = os.path.join(path, 'value_error_image')
+            create_folder(class_path)
+            logging.info('move value error image {} to {}'.format(file_path, class_path))
+            class_photo(file_path, class_path)
+
+
+if __name__ == '__main__':
+    main()
---
+++ label.py
@@ -0,0 +1,54 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+import tensorflow as tf  # TF2
+from PIL import Image
+
+
+def load_labels(filename):
+    with open(filename, 'r') as f:
+        return [line.strip() for line in f.readlines()]
+
+
+def label_image(image='/tmp/grace_hopper.bmp',
+            model_file='/tmp/mobilenet_v1_1.0_224_quant.tflite',
+            label_file='/tmp/labels.txt',
+            input_mean=127.5,
+            input_std=127.5,
+            ):
+    interpreter = tf.lite.Interpreter(model_path=model_file)
+    interpreter.allocate_tensors()
+
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+
+    # check the type of the input tensor
+    floating_model = input_details[0]['dtype'] == np.float32
+
+    # NxHxWxC, H:1, W:2
+    height = input_details[0]['shape'][1]
+    width = input_details[0]['shape'][2]
+    img = Image.open(image).resize((width, height))
+
+    # add N dim
+    input_data = np.expand_dims(img, axis=0)
+
+    if floating_model:
+        input_data = (np.float32(input_data) - input_mean) / input_std
+
+    interpreter.set_tensor(input_details[0]['index'], input_data)
+
+    interpreter.invoke()
+
+    output_data = interpreter.get_tensor(output_details[0]['index'])
+    results = np.squeeze(output_data)
+
+    top_k = results.argsort()[-5:][::-1]
+    labels = load_labels(label_file)
+    for i in top_k:
+        if floating_model:
+            return float(results[i]), labels[i]
+        else:
+            return float(results[i] / 255.0), labels[i]
